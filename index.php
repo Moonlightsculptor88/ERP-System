e2016417a1e2f0fd30266d1848cae998b068ca10
Moonlightsculptor88 <mahipreetham@gmail.com>
2021-03-13 12:46:37 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,95 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./styles/styles.css" rel="stylesheet">
+    <title>ERP System</title>
+  </head>
+  <body>
+  <?php
+require_once 'sidebar.php';
+?>
+<div id="full-page">
+<?php
+require_once 'navbar.php';
+?>
+<!-- Recently visited -->
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-4  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 1</div>
+                </div>
+            </div>
+            <div class="col-lg-4  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 2</div>
+                </div>
+            </div>
+            <div class="col-lg-4 panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 3</div>
+                </div>
+            </div>
+        </div>
+        
+    
+     </div>
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-4  panel-pad-10">
+                <div class="panel panel-default ">
+                  <div class="panel-body ">Recently visited 4</div>
+                </div>
+            </div>
+            <div class="col-lg-4  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 5</div>
+                </div>
+            </div>
+            <div class="col-lg-4  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 6</div>
+                </div>
+            </div>
+        
+        
+    </div>
+     </div>
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-4  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 7</div>
+                </div>
+            </div>
+            <div class="col-lg-4  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 8</div>
+                </div>
+            </div>
+            <div class="col-lg-4  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Recently visited 9</div>
+                </div>
+            </div>
+        </div>
+        
+    
+     
+
+
+</div>
+  
+  </body>
+</html>
+
---
+++ student.php
@@ -0,0 +1,128 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="./styles/styles.css" rel="stylesheet">
+    <title>ERP System</title>
+  </head>
+  <body>
+  <?php
+require_once 'sidebar.php';
+?>
+<div id="full-page">
+<?php
+require_once 'navbar.php';
+?>
+<!-- Recently visited -->
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-6  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 1</div>
+                </div>
+            </div>
+            <div class="col-lg-6  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 2</div>
+                </div>
+            </div>
+            
+            
+        </div>
+        
+    
+     </div>
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-6  panel-pad-10">
+                <div class="panel panel-default ">
+                  <div class="panel-body ">Student 3</div>
+                </div>
+            </div>
+            <div class="col-lg-6  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 4</div>
+                </div>
+            </div>
+
+    
+    </div>
+     </div>
+
+
+     <div class="container">
+        <div class="row">
+            <div class="col-lg-6  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 5</div>
+                </div>
+            </div>
+            <div class="col-lg-6  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 6</div>
+                </div>
+            </div>
+
+            <div class="container">
+        <div class="row">
+            <div class="col-lg-6  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 7</div>
+                </div>
+            </div>
+            <div class="col-lg-6  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 8</div>
+                </div>
+            </div>
+
+            <div class="container">
+        <div class="row">
+            <div class="col-lg-6  panel-pad-10">
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 9</div>
+                </div>
+            </div>
+            <div class="col-lg-6  panel-pad-10"> 
+                <div class="panel panel-default">
+                  <div class="panel-body">Student 10</div>
+                </div>
+            </div>
+
+            <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item ">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+            
+        
+    
+
+</div>
+  
+  </body>
+</html>
+
